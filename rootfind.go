b861a5cb8981294cbfb40cb9b932566d7302311b
Beng Zhee <bengz09@gmail.com>
2017-05-07 23:54:04 +0800

    Update rootfind.go

--- rootfind.go
+++ rootfind.go
@@ -7,7 +7,7 @@ type Evalfunc func(float64) float64
 func Secant(fn Evalfunc, x0 float64, tol float64) float64 {
     var next_f, x1 float64
 
-	if x0 >= 0 {
+	if x0 >= 0 {		
 		x1 = x0 * (1 + 1e-4) + 1e-4
 	} else {
 		x1 = x0 * (1 + 1e-4) - 1e-4
@@ -32,34 +32,21 @@ func Bisect(fn Evalfunc, left float64, right float64, tol float64) (float64, err
 	fleft := fn(left)
 	fright := fn(right)
 	if fleft == 0 {
-
 		return left, nil
-
 	} else if fright == 0 {
-
 		return right, nil
-
 	} else if fleft * fright > 0 {
-		
 		return -1, errors.New("Signs are the same for given bracketing interval")
-
 	} else {
-
 		fcentre = tol
 		for fcentre >= tol {
-
 			centre = (left + right) / 2
 			fcentre = fn(centre)
-			
 			if fcentre * fleft > 0 {
-
 				left = centre
-				
 			} else {
-
-				right = centre
-				
-			}
+				right = centre	
+			}	
 		}
 	}
 	
